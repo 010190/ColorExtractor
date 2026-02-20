701dc01925a719a1169071615cb9b06729ea77f5
010190 <olczakmichal4@gmail.com>
2026-02-20 12:22:38 +0100

    kmeans

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 import os
-from os import path
-from collections import defaultdict
-
 import numpy as np
 from PIL import Image, ImagePalette, ImageColor
-from PIL.ImageColor import getcolor
 import pandas as pd
 from flask import *
-import webcolors
-import easygui
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
 from flask_wtf.file import FileField
 from webcolors import hex_to_name, rgb_to_name, name_to_hex
-from werkzeug.utils import secure_filename
 import webcolors
 from flask_bootstrap import Bootstrap4
-from matplotlib.colors import hex2color, XKCD_COLORS
+from sklearn.cluster import KMeans
 
 
 class UploadForm(FlaskForm):
     file = FileField()
     number = IntegerField()
+
 
 def closest_colour(requested_colour):
     distances = {}
@@ -50,53 +44,39 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['UPLOAD_FOLDER'] = "static"
 
 
-def rgb_to_hex(r, g, b):
-    return ('{:02X}' * 3).format(r, g, b)
-
-
 def im_process(filename):
-    raw_list = []
-    image = np.array(Image.open(filename))
-
-    for x in image:
-
-        for y in x:
-            raw_list.append(y)
-
-    c_list = [tuple([item[0].item(), item[1].item(), item[2].item()]) for item in raw_list]
-
-    names_list = [get_colour_name(color)[1] for color in c_list]
-    color_series1 = pd.DataFrame({"color": c_list, "name": names_list, "count": 0})
-    color_series1["count"] = color_series1.groupby("name")["name"].transform('count')
-    new_df = color_series1.drop_duplicates(subset=["name"])
+    image = Image.open(filename).resize((120,120)).convert("RGB")
+    image = np.array(image)
+    image_kmeans = image.reshape((-1,3))
+    kmeans = KMeans(n_clusters=10, random_state=0, n_init="auto").fit(image_kmeans)
+    rgb_centroids = tuple((kmeans.cluster_centers_).astype(int))
+    colors_name = [get_colour_name(color)[1] for color in rgb_centroids]
+    new_df = pd.DataFrame({"name":colors_name, "color":rgb_centroids,})
+    color_set = set(list(kmeans.labels_))
+    count = [list(kmeans.labels_).count(color) for color in color_set]
+    new_df["count"] = count
+    new_df["hex"]= [name_to_hex(color) for color in new_df["name"].values]
     constant = new_df["count"].sum()
 
-    new_df["percentage"] = (new_df["count"] / constant) * 100
-    color_hex = [name_to_hex(color) for color in new_df["name"].values]
-    new_df["hex"] = color_hex
+    new_df["percentage"] = ((new_df["count"] / constant) * 100).round(2)
+    print(count)
     print(new_df)
 
-    return new_df.sort_values(by=["percentage"], ascending=False)[:10]
+    return new_df.sort_values(by=["percentage"], ascending=False)
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    colors_list = []
     new_df = pd.DataFrame(data={})
     form = UploadForm()
 
     if form.validate_on_submit():
         f = request.files['file']
-        # filename = secure_filename(form.file.data.filename)
-        # f.save(f.filename)
+        n = request.form.get("number")
+        print(n)
         f.save(os.path.join("static", f.filename))
-
         new_df = im_process(f)
-        # new_df = im_process(f"C:/Users/olcza/Desktop/{filename}")
-        # color_hex = [name_to_hex(color) for color in colors_list]
-        print(colors_list)
-        length = len(new_df.index)
-        return render_template("index.html", new_df=new_df, form=form, length=length, file=f)
+        return render_template("index.html", new_df=new_df, form=form, file=f)
     return render_template("index.html", form=form, new_df=new_df)
 
 
